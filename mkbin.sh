13a17ee9da26b9920d107a538bf24a8148b07f02
Andy Kluger <andykluger@gmail.com>
2019-01-15 00:41:23 -0500

    add build script using buildah rather than docker

---
+++ mkbin.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+# To run this container unprivileged, as a non-root user "myuser":
+# ----------------------------------------------------------------
+# /etc/subuid should contain something like:
+# myuser:100000:65536
+#
+# /etc/subgid should contain something like:
+# myuser:100000:65536
+#
+# To enable for the current session, run:
+# sudo sysctl kernel.unprivileged_userns_clone=1
+#
+# To enable for subsequent sessions,
+# /etc/sysctl.d/96-userns.conf should contain:
+# kernel.unprivileged_userns_clone = 1
+
+ctnr=`buildah from docker://inn0kenty/pyinstaller-alpine:3.7`
+buildah run -v "$PWD/essex:/src" $ctnr -- /pyinstaller/pyinstaller.sh -F essex.py
+buildah rm $ctnr
+rm -rf essex/__pycache__ essex/build essex/essex.spec
