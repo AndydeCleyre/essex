a9d866c22554938799f7f52d169aff7496700bb8
Andy Kluger <andykluger@gmail.com>
2019-01-19 14:59:27 -0500

    more version replacement

--- vbump.sh
+++ vbump.sh
@@ -3,4 +3,5 @@
 sed -E -i "s/^(__version__ = ')(\.|[0-9])*('.*)/\1$1\3/" essex/__init__.py
 sed -E -i "s/^( *VERSION = ')(\.|[0-9])*('.*)/\1$1\3/" essex/essex.py
 sed -E -i "s/^(version=).*/\1$1/" mkbin.sh
+sed -E -i "s:(/releases/download/)(\.|[0-9])*(/essex-)(\.|[0-9])*(-):\1$1\3$1\5:" README.rst
 echo "Remember to update the help output in README.rst"
