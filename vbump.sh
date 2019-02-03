d54eb5418bc8281748698081566096766a08605b
Andy Kluger <andykluger@gmail.com>
2019-02-02 23:53:36 -0500

    add creation of full docs to release procedure (docs/help-all.txt)

--- vbump.sh
+++ vbump.sh
@@ -4,4 +4,5 @@ sed -E -i "s/^(__version__ = ')(\.|[0-9])*('.*)/\1$1\3/" essex/__init__.py
 sed -E -i "s/^( *VERSION = ')(\.|[0-9])*('.*)/\1$1\3/" essex/essex.py
 sed -E -i "s/^(version=).*/\1$1/" mkbin.sh
 sed -E -i "s:(/releases/download/)(\.|[0-9])*(/essex-)(\.|[0-9])*(-):\1$1\3$1\5:" README.rst
+./essex/essex.py --help-all | sed 's/essex.py/essex/' > docs/help-all.txt
 echo "Remember to update the help output in README.rst"
