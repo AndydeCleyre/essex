af8e037db486d4a3e07739f451bedfa3f54378e6
Andy Kluger <andykluger@gmail.com>
2019-02-08 14:13:10 -0500

    add python license to binary distribution

--- mkbin.sh
+++ mkbin.sh
@@ -19,13 +19,16 @@
 # install fuse-overlayfs
 
 version=1.1.0
+rm -rf essex/dist/*
 ctnr=`buildah from docker://inn0kenty/pyinstaller-alpine:3.7`
 buildah run -v "$PWD/essex:/src" $ctnr -- /pyinstaller/pyinstaller.sh -F essex.py
 buildah run -v "$PWD/essex:/src" $ctnr -- /pyinstaller/pyinstaller.sh -F essex_complete.py -n _essex
 buildah rm $ctnr
 rm -rf essex/__pycache__ essex/build essex/*.spec
 cd essex/dist
-wget -O plumbum_license "https://raw.githubusercontent.com/tomerfiliba/plumbum/master/LICENSE"
-tar cfJ essex-$version-x86_64.tar.xz essex _essex plumbum_license
+mkdir -p ESSEX_LICENSES
+wget -O ESSEX_LICENSES/PLUMBUM "https://raw.githubusercontent.com/tomerfiliba/plumbum/master/LICENSE"
+wget -O ESSEX_LICENSES/PYTHON "https://raw.githubusercontent.com/python/cpython/master/LICENSE"
+tar cfJ essex-$version-x86_64.tar.xz essex _essex ESSEX_LICENSES
 pwd
 ls -lh
